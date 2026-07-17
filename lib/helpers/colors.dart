475aa24629f9206a55213b535f7e5afc0f34578a
Mutinda Boniface <boniface.info@gmail.com>
2026-07-17 09:46:47 +0300

    wip

--- lib/helpers/colors.dart
+++ lib/helpers/colors.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color scaffoldBackground = Color(0xFFF5F5F5);
   static const Color appBlue = Color(0xFF0875F8);
+  static const Color topBar = Color(0xFF092A5E);
+  static const Color statusBar = Color(0xFF061D42);
+  static const Color topBarControl = Color(0xFF1A447A);
+  static const Color topBarBorder = Color(0xFF5F84B2);
   static const Color darkText = Color(0xFF111827);
   static const Color mutedText = Color(0xFF667085);
   static const Color inputBorder = Color(0xFFD0D5DD);
