27015e97046fbccd717d5d8cb285608347d2494a
Mutinda Boniface <boniface.info@gmail.com>
2026-07-29 08:03:44 +0300

    service area to order area

--- lib/helpers/config/index.dart
+++ lib/helpers/config/index.dart
@@ -4,6 +4,6 @@ abstract class AppConfig {
   static String get baseUrl {
     return kReleaseMode
         ? 'https://api.pesatime.com/inventory/stock-app/v1/'
-        : 'https://cb60-102-217-4-34.ngrok-free.app/inventory/stock-app/v1/';
+        : 'https://cd51-102-217-4-34.ngrok-free.app/inventory/stock-app/v1/';
   }
 }
